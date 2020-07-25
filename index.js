5e98b1066d57b5409927ce7489712746af0ad2f7
TheGreatRambler <aehart684351@gmail.com>
2020-07-25 17:35:43 -0400

    Fix proxy https errors, need to fix requests

--- index.js
+++ index.js
@@ -1,47 +1,113 @@
-var Proxy = require("http-mitm-proxy");
+const Proxy = require("http-mitm-proxy");
+const path = require("path");
+const net = require("net");
+const msgpack = require("@msgpack/msgpack");
+
 var proxy = Proxy();
 
+proxy.onConnect(function(req, socket, head) {
+	console.log("CONNECT: " + req.url);
+
+	var host = req.url.split(":")[0];
+	var port = req.url.split(":")[1];
+
+	console.log('Tunnel to', req.url);
+	var conn = net.connect({
+		port: port,
+		host: host,
+		allowHalfOpen: true
+	}, function() {
+		conn.on('finish', () => {
+			socket.destroy();
+		});
+
+		socket.on('close', () => {
+			conn.end();
+		});
+
+		socket.on('data', (data) => {
+			console.log("FROM SOCKET: " + data.toString("ascii") + "\n");
+		});
+
+		conn.on('data', (data) => {
+			console.log("FROM CONN: " + data.toString("ascii") + "\n");
+		});
+
+		socket.write('HTTP/1.1 200 OK\r\n\r\n', 'UTF-8', function() {
+			conn.pipe(socket);
+			socket.pipe(conn);
+		})
+	});
+
+	conn.on('error', function(err) {
+		filterSocketConnReset(err, 'PROXY_TO_SERVER_SOCKET');
+	});
+	socket.on('error', function(err) {
+		filterSocketConnReset(err, 'CLIENT_TO_PROXY_SOCKET');
+	});
+});
+
+// Since node 0.9.9, ECONNRESET on sockets are no longer hidden
+function filterSocketConnReset(err, socketDescription) {
+	if (err.errno === 'ECONNRESET') {
+		console.log('Got ECONNRESET on ' + socketDescription + ', ignoring.');
+	} else {
+		console.log('Got unexpected error on ' + socketDescription, err);
+	}
+}
+
+
 proxy.onError(function(ctx, err) {
-	console.error("proxy error:", err);
+	console.error("ERROR:", err);
 });
 
 proxy.onRequest(function(ctx, callback) {
-	console.log("Request: ", ctx.clientToProxyRequest.headers);
+	console.log('REQUEST: http://' + ctx.clientToProxyRequest.headers.host + ctx.clientToProxyRequest.url);
 
-	var chunks = [];
-	ctx.onResponse(function(ctx, callback) {
-		if (ctx.clientToProxyRequest.headers.host === "ctest.cdn.nintendo.net") {
-			console.log("Set header for internet test");
-			//ctx.proxyToClientResponse.header["Content-Type"] = "text/plain";
-			ctx.proxyToClientResponse.setHeader("X-Organization", "Nintendo");
-		}
+	if (ctx.clientToProxyRequest.headers.host === "ctest.cdn.nintendo.net") {
+		ctx.proxyToClientResponse.setHeader("X-Organization", "Nintendo");
+		ctx.proxyToClientResponse.setHeader("Content-Type", "text/plain");
+		ctx.proxyToClientResponse.statusCode = 200;
+		ctx.proxyToClientResponse.end('ok');
+	} else if (ctx.clientToProxyRequest.headers.host === "conntest.nintendowifi.net") {
+		ctx.proxyToClientResponse.setHeader("X-Organization", "Nintendo");
+		ctx.proxyToClientResponse.setHeader("Content-Type", "text/html");
+		ctx.proxyToClientResponse.statusCode = 200;
+		ctx.proxyToClientResponse.end('<!DOCTYPE html PUBLIC " - //W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html><head><title>HTML Page</title></head><body bgcolor="#FFFFFF">This is test.html page</body></html>');
+	} else {
+		var chunks = [];
+		ctx.onResponse(function(ctx, callback) {
+			return callback(null);
+		});
+
+		ctx.onResponseData(function(ctx, chunk, callback) {
+			chunks.push(chunk);
+			//chunk = new Buffer(chunk.toString().replace(/<h3.*?<\/h3>/g, '<h3>Pwned!</h3>'));
+			return callback(null, chunk);
+		});
+
+		ctx.onResponseEnd(function(ctx, callback) {
+			console.log("REQUEST END", (Buffer.concat(chunks)).toString(), ctx.proxyToClientResponse);
+			return callback(null);
+		});
+
 		return callback();
-	});
-
-	ctx.onResponseData(function(ctx, chunk, callback) {
-		chunks.push(chunk);
-		//chunk = new Buffer(chunk.toString().replace(/<h3.*?<\/h3>/g, '<h3>Pwned!</h3>'));
-		return callback(null, chunk);
-	});
-
-	ctx.onResponseEnd(function(ctx, callback) {
-		console.log("REQUEST END", (Buffer.concat(chunks)).toString(), ctx.proxyToClientResponse);
-		return callback();
-	});
-
-	return callback();
+	}
 });
 
+/*
 proxy.onCertificateRequired = function(hostname, callback) {
-	console.log("Keys needed I guess");
-	// Oh no
-	/*return callback(null, {
-	  keyFile: path.resolve('/ca/certs/', hostname + '.key'),
-	  certFile: path.resolve('/ca/certs/', hostname + '.crt')
-	});
-	*/
-};
+		console.log("Keys needed I guess");
+		return callback(null, {
+			keyFile: path.join(__dirname, "keyfile.key"),
+			certFile: path.join(__dirname, "certfile.cert")
+		});
+	}
+*/
+
 
 proxy.listen({
-	port: 7654
+	port: 8080
 });
+
+console.log("Started proxy server");
